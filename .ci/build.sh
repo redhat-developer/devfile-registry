8c2576a30c4f83d6680b581b0ded3b3c992e964d
John Collier <jcollier@redhat.com>
2021-07-05 11:41:46 -0400

    Use Apache 2.0 license
    Signed-off-by: John Collier <jcollier@redhat.com>

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,12 +1,3 @@
-#
-# Copyright (c) 2021 Red Hat, Inc.
-# This program and the accompanying materials are made
-# available under the terms of the Eclipse Public License 2.0
-# which is available at https://www.eclipse.org/legal/epl-2.0/
-#
-# SPDX-License-Identifier: EPL-2.0
-#
-
 #!/usr/bin/env bash
 # This script downloads the registry build tools and builds up this repository
 # This script runs on both the GitHub action CI and the CICD for the hosted registry
